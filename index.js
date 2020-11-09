c9a33e4972620d3d5fc6f828cdb54b60e6f66639
Ariful4593 <arifkhagoria2015@gmail.com>
2020-11-09 10:16:21 +0600

    Code updated

--- index.js
+++ index.js
@@ -1,82 +1,106 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 require('dotenv').config()
-
-const ObjectId = require('mongodb').ObjectId
-
-
+const port = 4000;
 const app = express();
+
+
 app.use(bodyParser.json())
-app.use(cors())
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
-
+app.use(cors());
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const volunteerCollection = client.db("volunteerNetwork").collection("volunteer");
-
-    const connectedCollection = client.db("volunteerNetwork").collection("connected");
-
-    const volunteerRegister = client.db("volunteerNetwork").collection("registerList");
-
-    app.post('/volunteerRegisterData', (req, res) => {
-        const volData = req.body;
-        volunteerRegister.insertOne(volData)
-        .then(result =>{
-            res.send(result.insertedCount > 0);
-        })
-    })
     
-    app.get('/getVolunteerData', (req, res) =>{
-        volunteerRegister.find({})
-        .toArray( (err, documents) =>{
-            res.send(documents)
-        })
-    })
+    const volunteerCollection = client.db("volunteerNetworkFinal").collection("volunteerFinalDB");
 
-    app.post('/addCart', (req, res) =>{
-        
-        const card = req.body;
-        connectedCollection.insertOne(card)
+    const volunteerRegister = client.db("volunteerNetworkFinal").collection("volunteerRegisterData");
+
+    const volunteerSelected = client.db("volunteerNetworkFinal").collection("volunteerSelectedCard");
+
+    app.post('/addVolunteer', (req, res) => {
+        const data = req.body;
+        volunteerRegister.insertOne(data)
         .then(result => {
             res.send(result.insertedCount > 0)
         })
     })
 
-    app.get('/volunteerSelected', (req, res) =>{
-        console.log(req.query.email)
-        connectedCollection.find({email: req.query.email})
-        .toArray( (err, documents) =>{
-            res.send(documents)
-        }) 
+    app.post('/addVolunteerSelected', (req, res) => {
+        const data = req.body;
+        volunteerSelected.insertOne(data)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
     })
 
-    app.delete("/delete/:id", (req, res) => {
-        console.log(req.params.id)
-        connectedCollection.deleteOne({ _id: req.params.id })
-            .then((result) => {
-                console.log("Result successfully",result)
-                console.log(result.deletedCount)
-                res.send(result.deletedCount > 0);
-            })
-            
-    })
-    
-    app.get('/volunteerCard', (req, res) =>{
-        volunteerCollection.find({})
+    app.get('/volunteerSelectedInfo', (req, res) => {
+        volunteerSelected.find({email: req.query.email})
         .toArray( (err, documents) =>{
             res.send(documents)
-        }) 
+        })
+    })
+
+    
+
+    app.get('/getVolunteerRegisterInfo', (req, res) =>{
+        volunteerRegister.find({})
+        .toArray( (err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    app.post('/addDatabase', (req, res) =>{
+        const card = req.body;
+        volunteerCollection.insertMany(card)
+        .then(result =>{
+            res.send(result.insertedCount > 0)
+        })
+    })
+    app.get('/getDatabase', (req, res) =>{
+        volunteerCollection.find({})
+        .toArray((err, documents) =>{
+            res.send(documents)
+        })
+    })
+
+    
+
+    app.delete('/delete/:id', (req, res) =>{
+        volunteerSelected.deleteOne({_id: req.params.id})
+        .then(result => {
+            res.send(result.deletedCount > 0)
+        })
+    })
+
+    app.delete('/deleteRegister/:id', (req, res) =>{
+        console.log(req.params.id)
+        volunteerRegister.deleteOne({_id: ObjectId(req.params.id)})
+        .then(result => {
+            res.send(result.deletedCount > 0)
+        })
+    })
+
+    app.get('/tasks', (req, res) =>{
+        const searchItem = req.query.search;
+        console.log(searchItem)
+        volunteerCollection.find({title: {$regex: searchItem}})
+        .toArray( (err, documents) =>{
+            res.send(documents)
+        } )
+    })
+
+
+    app.get('/adminArea', (req, res) =>{
+        res.send("Hey i am admin")
     })
 });
 
+app.get('/', (req, res) => {
+    res.send("Hello I am Ariful ISALM");
+})
 
 
-
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || port)
